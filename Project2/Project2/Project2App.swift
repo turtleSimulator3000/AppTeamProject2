24cc22b8893d625114367e15df7d27e212238893
Jeffrey Hu <jeffreyh3000@gmail.com>
2026-02-24 12:37:03 -0500

    init

---
+++ Project2/Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by Jeffrey Hu on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
